76f4f279490a092356e330416f9c48ed48dc45f8
Alex Gilbert <agilbert7911@gmail.com>
2014-08-05 19:54:44 -0400

    Added rake assets

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-
+  
   get '/contact' => 'static_pages#contact'
   get '/about' => 'static_pages#about'
   get '/home' => 'static_pages#home'
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,61 +1,60 @@
 require 'spec_helper'
-
 describe "Static pages" do
 
-  describe "Home page" do 
+	describe "Home page" do 
 
-	it "should have the content 'Application Name'" do
-  	  visit root_path
-	  expect(page).to have_content('Application Name')
+		it "should have the content 'Application Name'" do
+  	  		visit root_path
+	  		expect(page).to have_content('Application Name')
+		end
+
+		it "should have the base title" do
+	  		visit root_path
+	  		expect(page).to have_title("Application Name")
+		end
+
+		it "should not have a custom page title" do
+	  		visit root_path
+	  		expect(page).not_to have_title('| Home')
+		end
+  	end
+
+  	describe "Help page" do
+
+		it "should have the content 'Help'" do
+		  visit help_path
+		  expect(page).to have_content('Help')
+		end
+
+		it "should have the title 'Help'" do
+		  visit help_path
+		  expect(page).to have_title("Application Name | Help")
+		end
+  	end
+
+	describe "About page" do
+
+		it "should have the content 'About Us'" do
+		  	visit about_path
+		  	expect(page).to have_content('About Us')
+		end
+
+		it "should have the title 'About Us'" do
+		  	visit about_path
+		  	expect(page).to have_title("Application Name | About Us")
+		end
 	end
 
-	it "should have the base title" do
-	  visit root_path
-	  expect(page).to have_title("Application Name")
-	end
+  	describe "Contact page" do
 
-	it "should not have a custom page title" do
-	  visit root_path
-	  expect(page).not_to have_title('| Home')
-	end
-  end
+		it "should have the content 'Contact'" do
+	  		visit contact_path
+	  		expect(page).to have_content('Contact')
+		end
 
-  describe "Help page" do
-
-	it "should have the content 'Help'" do
-	  visit help_path
-	  expect(page).to have_content('Help')
-	end
-
-	it "should have the title 'Help'" do
-	  visit help_path
-	  expect(page). to hae_title("Application Name | Help")
-	end
-  end
-
-  describe "About page" do
-
-	it "should have the content 'About Us'" do
-	  visit about_path
-	  expect(page).to have_content('About Us')
-	end
-
-	it "should have the title 'About Us'" do
-	  visit about_path
-	  expect(page).to have_title("Application Name | About Us")
-	end
-  end
-
-  describe "Contact page" do
-
-	it "should have the content 'Contact'" do
-	  visit contact_path
-	  expect(page).to have_content('Contact')
-	end
-
-	it "should have the title 'Contact'" do
-	  visit contact_path
-	  expect(page).to have_title("Application Name | Contact")
-	end
-  end
+		it "should have the title 'Contact'" do
+	  		visit contact_path
+	  		expect(page).to have_title("Application Name | Contact")
+		end
+  	end
 end
