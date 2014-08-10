67644071ea72ec78696e7c90df1d23aa7b067b12
Alex Gilbert <agilbert7911@gmail.com>
2014-08-10 17:23:02 -0400

    added password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
 	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
 		uniqueness: { case_sensitive: false }
 	validates :name, presence: true, length: { maximum: 50 }
-
+	validates :password, length: { minimum: 6 }
 	
 	before_save { self.email = email.downcase }
 end
