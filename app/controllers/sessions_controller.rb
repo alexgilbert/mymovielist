a90b251b18b73a792fbfc812e12625222441ea4d
Alex Gilbert <agilbert7911@gmail.com>
2014-08-14 17:35:31 -0400

    Added user login and session helper with tests

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      signin_in user
+      sign_in user
       redirect_to user
     else
       flash.now[:error] = 'Invalid email/password combination'
@@ -15,5 +15,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    sign_out
+    redirect_to root_url
   end
 end
