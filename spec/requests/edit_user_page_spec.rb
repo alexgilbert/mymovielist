63afa6354c2acd330d02b653d3b49eac9dd39513
Alex Gilbert <agilbert7911@gmail.com>
2014-08-19 19:37:40 -0400

    Added test for editing users

--- spec/requests/edit_user_page_spec.rb
+++ spec/requests/edit_user_page_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe "Edit User page" do
+describe "Edit User page", type: :request do
 
   subject { page }
 
@@ -40,6 +40,19 @@ describe "Edit User page" do
       it { should have_link('Sign out', href: signout_path) }
       specify { expect(user.reload.name).to eq new_name }
       specify { expect(user.reload.email).to eq new_email }
+    end
+  
+    describe "forbidden attributes" do
+      let(:params) do
+        { user: { admin: true, password: user.password, password_confirmation: user.password } }
+      end
+
+      before do
+        sign_in user, no_capybara: true
+        patch user_path(user), params
+      end
+
+      specify { expect(user.reload).not_to be_admin }
     end
   end
 end
