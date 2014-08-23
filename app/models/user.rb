e68d546b912d3ad45963978c55c482b6de298602
Alex Gilbert <agilbert7911@gmail.com>
2014-08-23 17:28:53 -0400

    Added the destruction of all assocated microposts when user is deleted

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  has_many :microposts
+  has_many :microposts, dependent: :destroy
   has_secure_password
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
