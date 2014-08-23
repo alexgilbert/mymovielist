64b72c041c830c2ecf8f6158003a3b21822b593e
Alex Gilbert <agilbert7911@gmail.com>
2014-08-23 17:24:12 -0400

    ordered the microposts with default_scope DESC on created_at

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -129,4 +129,18 @@ describe User do
       expect(@user.reload.email).to eq mixed_case_email.downcase
     end
   end	
+
+  describe "micropost associations" do 
+    before { @user.save }
+    let!(:older_micropost) do
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
+    end
+    let!(:newer_micropost) do
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
+    end
+    
+    it "should have the right microposts in the right order" do
+      expect(@user.microposts.to_a).to eq [newer_micropost, older_micropost]
+    end
+  end
 end
