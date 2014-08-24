75f7a23ba3d4b16a26c6614ec000385beb7e70b3
Alex Gilbert <agilbert7911@gmail.com>
2014-08-23 20:45:58 -0400

    micropost#create page created

--- spec/requests/micropost_pages_spec.rb
+++ spec/requests/micropost_pages_spec.rb
@@ -1,10 +1,27 @@
-require 'rails_helper'
+require 'spec_helper'
 
-RSpec.describe "MicropostPages", :type => :request do
-  describe "GET /micropost_pages" do
-    it "works! (now write some real specs)" do
-      get micropost_pages_index_path
-      expect(response.status).to be(200)
-    end
-  end
+describe "Micropost page" do 
+	subject { page }
+
+	let(:user) { FactoryGirl.create(:user) }
+	before { sign_in user }
+
+	describe "micropost creation" do
+		before { visit root_path }
+
+		describe "with invalid information" do
+
+			it "should not create a micropost" do
+				expect { click_button "Post" }.not_to change(Micropost, :count)
+			end
+
+			describe "with valid information" do
+
+				before { fill_in 'micropost_content', with: "Lorem ipsum" }
+				it "should create a micropost" do
+					expect { click_button "Post" }.to change(Micropost, :count).by(1)
+				end
+			end
+		end
+	end
 end
