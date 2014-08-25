397821071b7cc95aca0e64da129a5f776d851ffe
Alex Gilbert <agilbert7911@gmail.com>
2014-08-25 16:54:39 -0400

    Added associations to user and relationship models

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -1,5 +1,19 @@
-require 'rails_helper'
+require 'spec_helper'
 
-RSpec.describe Relationship, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Relationship do
+  
+  let(:follower) { FactoryGirl.create(:user) }
+  let(:followed) { FactoryGirl.create(:user) }
+  let(:relationship) { follower.relationships.build(followed_id: followed.id) }
+
+  subject { relationship }
+
+  it { should be_valid }
+
+  describe "follower methods" do
+    it { should respond_to(:follower) }
+    it { should respond_to(:followed) }
+    its(:follower) { should eq follower }
+    its(:followed) { should eq followed }
+  end
 end
