21b2d421e0c11130846bb897cf6091ba7c0f112a
Alex Gilbert <agilbert7911@gmail.com>
2014-08-29 18:01:04 -0400

    Added follow form partial and follow/unfollow partials

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
   match '/signout',	to: 'sessions#destroy',	via: 'delete'
 
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
