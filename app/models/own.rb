b598c63a82fcb67e5637cc57fe1694b9c9ae803c
Alex Gilbert <agilbert7911@gmail.com>
2014-09-02 20:21:02 -0400

    Added Own test

--- app/models/own.rb
+++ app/models/own.rb
@@ -1,4 +1,7 @@
 class Own < ActiveRecord::Base
   belongs_to :user
   belongs_to :movie
+
+  validates :user_id, presence: true
+  validates :movie_id, presence: true
 end
--- spec/models/own_spec.rb
+++ spec/models/own_spec.rb
@@ -1,5 +1,27 @@
-require 'rails_helper'
+require 'spec_helper'
 
-RSpec.describe Own, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Own do
+
+  before do
+    @user = FactoryGirl.create(:user)
+    @movie = FactoryGirl.create(:movie)
+    @own = Own.new(user_id: @user.id, movie_id: @movie.id)
+  end
+
+  subject { @own }
+
+  it { should respond_to(:user_id) }
+  it { should respond_to(:movie_id) }
+  it { should respond_to(:user) }
+  it { should respond_to(:movie) }
+
+  describe "when user_id not present" do
+    before { @own.user_id = " " }
+    it { should_not be_valid }
+  end
+
+  describe "when movie_id not present" do
+    before { @own.movie_id = " " }
+    it { should_not be_valid }
+  end
 end
